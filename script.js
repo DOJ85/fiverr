e9864cfa5b7dd454c08a23808b7f79d8d1bac388
Unknown <florianpelloux85@gmail.com>
2018-11-10 07:45:29 +0100

    0.31

--- script.js
+++ script.js
@@ -55,9 +55,9 @@ function gotPos(position){
 function posFail(err){
     //err is a number
     let errors = {
-        1: 'No permission',
-        2: 'Unable to determine',
-        3: 'Took too long'
+        1:document.getElementById('location').innerHTML = 'Enable your Geolocation too see this section',
+        2:document.getElementById('location').innerHTML =  'An error as occured, please retry',
+        3:document.getElementById('location').innerHTML =  'Enable your Geolocation too see this section'
     }
     // document.querySelector('h1').textContent = errors[err];
 }
