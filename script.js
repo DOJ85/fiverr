1406c1801e7deee902eab6975846f3429c5d505a
Unknown <florianpelloux85@gmail.com>
2018-11-10 03:40:37 +0100

    0.23

--- script.js
+++ script.js
@@ -15,3 +15,20 @@ function closeCover(){
   document.getElementById("side-bar").classList.remove("active");
   document.getElementById("toggleSideBar").classList.remove("active");
 }
+
+// window.fetch('http://api.openweathermap.org/data/2.5/weather?q=London&units=metric&lang=en&appid=62133f60aa740596c21f37130a69970c')
+//   .then(res => res.json())
+//   .then(resJson => console.log(resJson))
+
+var x = document.getElementById("demo");
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+function showPosition(position) {
+    x.innerHTML = "Latitude: " + position.coords.latitude +
+    "<br>Longitude: " + position.coords.longitude;
+}
